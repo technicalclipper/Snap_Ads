654f009d6e80b431a6ca7d95969ce205420cf02b
technicalclipper <technicalclipper@gmail.com>
2025-04-05 03:58:04 +0800

    tested multibaas event query and rest api

--- web/app/api/deployadagent/route.tsx
+++ web/app/api/deployadagent/route.tsx
@@ -5,31 +5,29 @@ import { ethers } from "ethers";
 import fetch from 'node-fetch';
 
 export async function POST(req: NextRequest) {
-    const chain = 'ethereum'; 
-  const addressOrAlias = '0xd7FB59972869DdD555012cA7aD54b4b969b33ebd'; 
-  const contract = 'sendhi'; 
-  const method = 'sendhi';
-  const hostname = 'zev7quhwlbbqpgjd5vggxe326i.multibaas.com'; 
+  
 
-  const url = `https://${hostname}/api/v0/chains/${chain}/addresses/${addressOrAlias}/contracts/${contract}/methods/${method}`;
+ 
+    const eventQuery = 'publishad';
+    const query = new URLSearchParams({
+      offset: '0',
+      limit: '10'
+    }).toString();
+    const hostname = 'zev7quhwlbbqpgjd5vggxe326i.multibaas.com';
+    const resp = await fetch(
+      `https://${hostname}/api/v0/queries/${eventQuery}/results?${query}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzQzNzU5NTIwLCJqdGkiOiIyNWMyNDlmNS1kNTdkLTRjZWYtYjNlMy00YTg5ZmU2NzljYjUifQ.--O93RgkXL1pXPgMr0zAEmGEP5KdNXjdFiGuR1AilIc'
+        }
+      }
+    );
+  
+    const data = await resp.json();
+    console.log(data.result);
+  }
+  
 
-  console.log('Calling:', url); 
 
-  const resp = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.MULTIBAAS_API_KEY}` // Replace with your real token
-    },
-    body: JSON.stringify({
-      signature: 'sendhi(string)', 
-      args: ['hello from Multibaas!'], 
-      from: '0xa194D0531Fff63326BF39113bc456Cf8646e7422', 
-      signAndSubmit: true 
-    })
-  });
-
-  const data = await resp.json();
-  console.log('Response:', data);
-}
     
