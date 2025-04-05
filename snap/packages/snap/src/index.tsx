f0fe5890950af94387ffc6157ea4834644abc396
technicalclipper <technicalclipper@gmail.com>
2025-04-05 20:05:13 +0800

    Reinitialised snaps for ads

--- snap/packages/snap/src/index.tsx
+++ snap/packages/snap/src/index.tsx
@@ -1,7 +1,7 @@
 import type { OnTransactionHandler } from "@metamask/snaps-sdk";
 import { } from "@metamask/snaps-sdk";
 import { Heading,Box,Text,Image} from "@metamask/snaps-sdk/jsx";
-import svgIcon from "./images/images.png";
+
 
 
 export const onTransaction: OnTransactionHandler = async ({
@@ -13,7 +13,6 @@ export const onTransaction: OnTransactionHandler = async ({
     content: (<Box>
       <Heading>Hello world!</Heading>
       <Text>Namma thaa Finalist </Text>
-      <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPuAf-qIiiJdjHeQCjfjBqTeOwtQtVZ9IVKw&s" />
-    </Box>),
+    </Box>)
   };
 };
